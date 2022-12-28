764e94b0fd66c8c462c42afe3c34c62f03811d0b
bambamboole <manuel.christlieb@xentral.com>
2022-12-28 17:36:33 +0100

    Extend utils tests

--- pkg/utils_test.go
+++ pkg/utils_test.go
@@ -28,3 +28,32 @@ func Test_folderExists(t *testing.T) {
 		assert.False(t, directoryExists(path.Join(path.Dir(filename), "fixtures", "test_project", "dummy_file.txt")))
 	})
 }
+
+func Test_isGitRepo(t *testing.T) {
+	t.Run("it can check if a folder is a git repository", func(t *testing.T) {
+		_, filename, _, _ := runtime.Caller(0)
+		assert.True(t, isGitRepo(path.Join(path.Dir(filename), "fixtures", "test_project")))
+	})
+	t.Run("cross check with non git folder", func(t *testing.T) {
+		_, filename, _, _ := runtime.Caller(0)
+		assert.False(t, isGitRepo(path.Join(path.Dir(filename), "fixtures")))
+	})
+}
+
+func Test_isRoot(t *testing.T) {
+	t.Run("it checks if the passed string is the root folder", func(t *testing.T) {
+		assert.True(t, isRoot("/"))
+		assert.False(t, isRoot("/foobar"))
+	})
+}
+
+func Test_DetectProjectPath(t *testing.T) {
+	t.Run("It detects the project path based on git folder", func(t *testing.T) {
+		_, filename, _, _ := runtime.Caller(0)
+
+		projectPath := DetectProjectPath(path.Join(path.Dir(filename), "fixtures", "test_project", "subdir", "subdir"))
+		_, projectPath = path.Split(projectPath)
+
+		assert.Equal(t, "test_project", projectPath)
+	})
+}
